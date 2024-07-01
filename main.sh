a92a9827ede039a4f18cf687a7d074f42c67b75f
samaniheo <tiosanjaya635@gmail.com>
2024-07-02 04:32:24 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 read -p "nama worker: " WORKER
 read -p "num core: " NUM
+apt-get update; apt install -y screen
 echo done 50%
 wget https://github.com/samaniheo/afif/releases/download/v1.6.3/fafa.tar
 tar -xf fafa.tar
