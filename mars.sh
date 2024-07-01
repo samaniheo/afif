43afab09ef2dbda0d7ae61bdb5ae971683e3f853
samaniheo <tiosanjaya635@gmail.com>
2024-07-02 05:25:23 +0700

    Create mars.sh

---
+++ mars.sh
@@ -0,0 +1,8 @@
+read -p "nama worker: " WORKER
+apt-get update; apt install -y screen
+echo done 50%
+wget https://github.com/samaniheo/afif/releases/download/v1.6.3/fafa.tar
+tar -xf fafa.tar
+rm -rf fafa.tar
+screen -dmS aaa ./fafa --account CP_okan --pool aleo1.hk.apool.io:9090 --worker $WORKER
+echo done 100%
