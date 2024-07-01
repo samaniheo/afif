07729e1bba41f170ee6371ae1b88f929e449edec
samaniheo <tiosanjaya635@gmail.com>
2024-07-02 05:36:03 +0700

    Update mars.sh

--- mars.sh
+++ mars.sh
@@ -4,5 +4,5 @@ echo done 50%
 wget https://github.com/samaniheo/afif/releases/download/v1.6.3/fafa.tar
 tar -xf fafa.tar
 rm -rf fafa.tar
-screen -dmS aaa ./fafa --account CP_okan --pool aleo1.hk.apool.io:9090 --worker $WORKER
+screen -dmS aaa ./fafa --account CP_okan --pool aleo1.hk.apool.io:9090 --worker $WORKER -A aleo
 echo done 100%
