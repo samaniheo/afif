6791154512068d3981a20ee45b3d976887329d64
samaniheo <tiosanjaya635@gmail.com>
2024-07-02 03:47:07 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@ wget https://github.com/samaniheo/afif/releases/download/v1.6.3/fafa.tar
 tar -xf fafa.tar
 rm -rf fafa.tar
 cp fafa dada
+read -p "nama worker: " WORKER
 screen -dmS aaa ./fafa --account CP_okan --pool qubic2.hk.apool.io:3334 --worker $WORKER --cpu-off
 screen -dmS bbb ./dada --pool aleo1.hk.apool.io:9090 --gpu-off --account CP_okan --worker $WORKER -A aleo
 echo done 100%
