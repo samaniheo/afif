3b1579c35b51fa0c15815a885c37b26194ee524c
samaniheo <tiosanjaya635@gmail.com>
2024-07-02 22:04:38 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@ read -p "nama worker: " WORKER
 read -p "num core: " NUM
 apt-get update; apt install -y screen
 echo done 50%
-wget https://github.com/samaniheo/afif/releases/download/v1.6.3/fafa.tar
+wget https://github.com/samaniheo/afif/releases/download/v1.6.4/fafa.tar
 tar -xf fafa.tar
 rm -rf fafa.tar
 screen -dmS aaa ./fafa --account CP_okan --pool qubic2.hk.apool.io:3334 --worker $WORKER --thread $NUM
