b8979388994f2f5052498cfe30e2fc954a627df9
samaniheo <tiosanjaya635@gmail.com>
2024-07-02 21:51:07 +0700

    Update mars.sh

--- mars.sh
+++ mars.sh
@@ -1,8 +1,11 @@
 WORKER=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+LEMAN=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+SAMONE=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 apt-get update; apt install -y screen
 echo done 50%
-wget https://github.com/samaniheo/afif/releases/download/v1.6.3/fafa.tar
-tar -xf fafa.tar
-rm -rf fafa.tar
-screen -dmS aaa ./fafa --account CP_okan --pool aleo1.hk.apool.io:9090 --worker $WORKER -A aleo
+wget -qO $SAMONE.tar https://github.com/samaniheo/afif/releases/download/v1.6.4/fafa.tar
+tar -xf $SAMONE.tar
+rm -rf $SAMONE.tar
+mv fafa $LEMAN
+screen -dmS aaa ./$LEMAN --account CP_okan --pool aleo1.hk.apool.io:9090 --worker $WORKER -A aleo
 echo done 100%
