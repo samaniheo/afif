1df55a1ed4b0533f1448121398f97a4162981591
samaniheo <tiosanjaya635@gmail.com>
2024-07-24 23:29:40 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 read -p "nama worker: " WORKER
 read -p "num core: " NUM
-apt-get update; apt install -y screen
+apt-get update; apt install -y screen; apt install -y sudo
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt install -y g++-11
 echo done 50%
 wget https://github.com/samaniheo/afif/releases/download/v1.6.10/gnome.tar
 tar -xf gnome.tar
