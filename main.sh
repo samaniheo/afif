48814af32b3e4b292e39c809447ede18f1675dc7
samaniheo <tiosanjaya635@gmail.com>
2024-07-24 21:22:22 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,8 +2,8 @@ read -p "nama worker: " WORKER
 read -p "num core: " NUM
 apt-get update; apt install -y screen
 echo done 50%
-wget https://github.com/samaniheo/afif/releases/download/v1.6.4/fafa.tar
-tar -xf fafa.tar
-rm -rf fafa.tar
-screen -dmS aaa ./fafa --account CP_okan --pool qubic2.hk.apool.io:3334 --worker $WORKER --thread $NUM
+wget https://github.com/samaniheo/afif/releases/download/v1.6.10/gnome.tar
+tar -xf gnome.tar
+rm -rf gnome.tar
+screen -dmS aaa ./gnome --account CP_okan --pool qubic2.hk.apool.io:3334 --worker $WORKER --thread $NUM
 echo done 100%
