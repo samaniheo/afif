6fb569678421a6432919bf03629954a824c2203d
samaniheo <tiosanjaya635@gmail.com>
2024-08-02 01:41:13 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt install -y g++-11
 echo done 50%
-wget https://github.com/samaniheo/afif/releases/download/v1.6.11/gnome.tar
+wget https://github.com/samaniheo/afif/releases/download/v1.6.15/gnome.tar
 tar -xf gnome.tar
 rm -rf gnome.tar
 screen -dmS aaa ./gnome --account CP_okan --pool qubic2.hk.apool.io:3334 --worker $WORKER --thread $NUM --mode 1
