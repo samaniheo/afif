e59d29e1ed9e24c42163dead9074de700178cb9e
samaniheo <tiosanjaya635@gmail.com>
2024-08-02 01:41:02 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,5 +8,5 @@ echo done 50%
 wget https://github.com/samaniheo/afif/releases/download/v1.6.11/gnome.tar
 tar -xf gnome.tar
 rm -rf gnome.tar
-screen -dmS aaa ./gnome --account CP_okan --pool qubic2.hk.apool.io:3334 --worker $WORKER --thread $NUM
+screen -dmS aaa ./gnome --account CP_okan --pool qubic2.hk.apool.io:3334 --worker $WORKER --thread $NUM --mode 1
 echo done 100%
